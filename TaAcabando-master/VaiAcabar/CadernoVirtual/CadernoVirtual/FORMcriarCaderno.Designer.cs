6a5855dda6c9a07a2418ad46d871b134f7b0ab46
ThamiresIn <mlthami.mendes@gmail.com>
2017-11-28 14:43:02 -0200

    Form3

---
+++ TaAcabando-master/VaiAcabar/CadernoVirtual/CadernoVirtual/FORMcriarCaderno.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CadernoVirtual
+{
+    partial class FORMcriarCaderno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FORMcriarCaderno));
+            this.PANELcadastrar = new System.Windows.Forms.Panel();
+            this.lblConfirmarSenhaTurma = new System.Windows.Forms.Label();
+            this.TXTconfirmarsenhaturma = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TXTsenhaTurma = new System.Windows.Forms.TextBox();
+            this.TXTano = new System.Windows.Forms.TextBox();
+            this.TXTturma = new System.Windows.Forms.TextBox();
+            this.lblSenhaTurma = new System.Windows.Forms.Label();
+            this.lblAno = new System.Windows.Forms.Label();
+            this.lblTurma = new System.Windows.Forms.Label();
+            this.BTNcriarturma = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PANELcadastrar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PANELcadastrar
+            // 
+            this.PANELcadastrar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PANELcadastrar.BackColor = System.Drawing.Color.Transparent;
+            this.PANELcadastrar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PANELcadastrar.Controls.Add(this.lblConfirmarSenhaTurma);
+            this.PANELcadastrar.Controls.Add(this.TXTconfirmarsenhaturma);
+            this.PANELcadastrar.Controls.Add(this.label5);
+            this.PANELcadastrar.Controls.Add(this.pictureBox1);
+            this.PANELcadastrar.Controls.Add(this.TXTsenhaTurma);
+            this.PANELcadastrar.Controls.Add(this.TXTano);
+            this.PANELcadastrar.Controls.Add(this.TXTturma);
+            this.PANELcadastrar.Controls.Add(this.lblSenhaTurma);
+            this.PANELcadastrar.Controls.Add(this.lblAno);
+            this.PANELcadastrar.Controls.Add(this.lblTurma);
+            this.PANELcadastrar.Controls.Add(this.BTNcriarturma);
+            this.PANELcadastrar.Location = new System.Drawing.Point(268, 91);
+            this.PANELcadastrar.Name = "PANELcadastrar";
+            this.PANELcadastrar.Size = new System.Drawing.Size(604, 455);
+            this.PANELcadastrar.TabIndex = 6;
+            this.PANELcadastrar.Visible = false;
+            // 
+            // lblConfirmarSenhaTurma
+            // 
+            this.lblConfirmarSenhaTurma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblConfirmarSenhaTurma.AutoSize = true;
+            this.lblConfirmarSenhaTurma.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold);
+            this.lblConfirmarSenhaTurma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.lblConfirmarSenhaTurma.Location = new System.Drawing.Point(120, 208);
+            this.lblConfirmarSenhaTurma.Name = "lblConfirmarSenhaTurma";
+            this.lblConfirmarSenhaTurma.Size = new System.Drawing.Size(149, 18);
+            this.lblConfirmarSenhaTurma.TabIndex = 15;
+            this.lblConfirmarSenhaTurma.Text = "Confirmar Senha";
+            // 
+            // TXTconfirmarsenhaturma
+            // 
+            this.TXTconfirmarsenhaturma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TXTconfirmarsenhaturma.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTconfirmarsenhaturma.Location = new System.Drawing.Point(275, 204);
+            this.TXTconfirmarsenhaturma.Name = "TXTconfirmarsenhaturma";
+            this.TXTconfirmarsenhaturma.PasswordChar = '*';
+            this.TXTconfirmarsenhaturma.Size = new System.Drawing.Size(170, 22);
+            this.TXTconfirmarsenhaturma.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.label5.Location = new System.Drawing.Point(252, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(447, 73);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Criar Caderno";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 108);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TXTsenhaTurma
+            // 
+            this.TXTsenhaTurma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TXTsenhaTurma.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTsenhaTurma.Location = new System.Drawing.Point(275, 176);
+            this.TXTsenhaTurma.Name = "TXTsenhaTurma";
+            this.TXTsenhaTurma.PasswordChar = '*';
+            this.TXTsenhaTurma.Size = new System.Drawing.Size(170, 22);
+            this.TXTsenhaTurma.TabIndex = 11;
+            // 
+            // TXTano
+            // 
+            this.TXTano.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TXTano.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTano.Location = new System.Drawing.Point(275, 146);
+            this.TXTano.Name = "TXTano";
+            this.TXTano.Size = new System.Drawing.Size(118, 22);
+            this.TXTano.TabIndex = 10;
+            // 
+            // TXTturma
+            // 
+            this.TXTturma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TXTturma.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTturma.Location = new System.Drawing.Point(275, 118);
+            this.TXTturma.Name = "TXTturma";
+            this.TXTturma.Size = new System.Drawing.Size(170, 22);
+            this.TXTturma.TabIndex = 8;
+            // 
+            // lblSenhaTurma
+            // 
+            this.lblSenhaTurma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblSenhaTurma.AutoSize = true;
+            this.lblSenhaTurma.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold);
+            this.lblSenhaTurma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.lblSenhaTurma.Location = new System.Drawing.Point(210, 177);
+            this.lblSenhaTurma.Name = "lblSenhaTurma";
+            this.lblSenhaTurma.Size = new System.Drawing.Size(59, 18);
+            this.lblSenhaTurma.TabIndex = 7;
+            this.lblSenhaTurma.Text = "Senha";
+            // 
+            // lblAno
+            // 
+            this.lblAno.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblAno.AutoSize = true;
+            this.lblAno.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold);
+            this.lblAno.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.lblAno.Location = new System.Drawing.Point(179, 150);
+            this.lblAno.Name = "lblAno";
+            this.lblAno.Size = new System.Drawing.Size(91, 18);
+            this.lblAno.TabIndex = 6;
+            this.lblAno.Text = "Ano/Série";
+            // 
+            // lblTurma
+            // 
+            this.lblTurma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTurma.AutoSize = true;
+            this.lblTurma.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTurma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.lblTurma.Location = new System.Drawing.Point(195, 122);
+            this.lblTurma.Name = "lblTurma";
+            this.lblTurma.Size = new System.Drawing.Size(65, 18);
+            this.lblTurma.TabIndex = 4;
+            this.lblTurma.Text = "Turma";
+            // 
+            // BTNcriarturma
+            // 
+            this.BTNcriarturma.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BTNcriarturma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.BTNcriarturma.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTNcriarturma.FlatAppearance.BorderSize = 0;
+            this.BTNcriarturma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTNcriarturma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.BTNcriarturma.ForeColor = System.Drawing.SystemColors.Info;
+            this.BTNcriarturma.Location = new System.Drawing.Point(239, 291);
+            this.BTNcriarturma.Name = "BTNcriarturma";
+            this.BTNcriarturma.Size = new System.Drawing.Size(127, 41);
+            this.BTNcriarturma.TabIndex = 3;
+            this.BTNcriarturma.Text = "Criar";
+            this.BTNcriarturma.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 251);
+            this.panel1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.label1.Location = new System.Drawing.Point(61, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 75);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Para criar a sua turma:\r\n¨Informe a mesma\r\n¨Informe seu ano\r\n¨Escolha sua senha\r\n" +
+    "¨Confirme-a";
+            // 
+            // FORMcriarCaderno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1140, 637);
+            this.Controls.Add(this.PANELcadastrar);
+            this.Controls.Add(this.panel1);
+            this.Name = "FORMcriarCaderno";
+            this.Text = "FORMcriarCaderno";
+            this.PANELcadastrar.ResumeLayout(false);
+            this.PANELcadastrar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PANELcadastrar;
+        private System.Windows.Forms.Label lblConfirmarSenhaTurma;
+        private System.Windows.Forms.TextBox TXTconfirmarsenhaturma;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TXTsenhaTurma;
+        private System.Windows.Forms.TextBox TXTano;
+        private System.Windows.Forms.TextBox TXTturma;
+        private System.Windows.Forms.Label lblSenhaTurma;
+        private System.Windows.Forms.Label lblAno;
+        private System.Windows.Forms.Label lblTurma;
+        private System.Windows.Forms.Button BTNcriarturma;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
